9340a91b4fdb49b951f59c95113cfbdbdbb6f316
Raluca Pop <popralluca@gmail.com>
2020-10-31 13:34:41 +0200

    display products from json file

--- js-comfy-house/app.js
+++ js-comfy-house/app.js
@@ -6,9 +6,9 @@ const clearCartBtn = document.querySelector('.clear-cart');
 const cartDOM = document.querySelector('.cart');
 const cartOverlay = document.querySelector('.cart-overlay');
 const cartItems = document.querySelector('cart-items');
-const cartTotal = document.querySelector('cart-total');
-const cartContent = document.querySelector('cart-content');
-const productsDOM = document.querySelector('products-center');
+const cartTotal = document.querySelector('.cart-total');
+const cartContent = document.querySelector('.cart-content');
+const productsDOM = document.querySelector('.products-center');
 
 // main cart array
 
@@ -17,12 +17,52 @@ let cart = [];
 
 // getting the products
 class Products {
-
+    async getProducts() {
+        try {
+            let result = await fetch('products.json');
+            let data = await result.json();
+            let products = data.items;
+            //map and destructuring obj
+            products = products.map(item => {
+                const { title, price } = item.fields;
+                const { id } = item.sys;
+                const image = item.fields.image.fields.file.url;
+                return { title, price, id, image }
+            })
+            return products;
+        }
+        catch (error) {
+            console.log(error);
+        }
+    }
 }
 
 // display products
 class UI {
+    displayProducts(products) {
+        console.log(products);
 
+        let result = '';
+        products.forEach(product => {
+            result += `
+            <!-- single product -->
+             <article class="product">
+                <div class="img-container">
+                    <img src=${product.image} alt="product" class="product-img" />
+                    <button class="bag-btn" data-id=${product.id}>
+                        <i class="fas fa-shopping-cart"></i>
+                        add to cart
+                    </button>
+                </div>
+
+                <h3>${product.title}</h3>
+                <h4>$${product.price}</h4>
+            </article> 
+            <!-- end of single product -->
+           `;
+        });
+        productsDOM.innerHTML = result;
+    }
 }
 
 // local storage
@@ -35,4 +75,10 @@ document.addEventListener('DOMContentLoaded', () => {
     const ui = new UI();
     const products = new Products();
 
+    //get all products
+    products.getProducts().then(products =>
+        ui.displayProducts(products));
+
 });
+
+// am ramas la 1.47
